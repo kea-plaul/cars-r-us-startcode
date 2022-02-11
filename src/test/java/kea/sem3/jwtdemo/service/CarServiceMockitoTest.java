9feedb185fad8ef9093912259c10babeefbc8a59
jmunkc <jona04g8@stud.kea.dk>
2022-02-11 11:34:42 +0100

    stuff

--- src/test/java/kea/sem3/jwtdemo/service/CarServiceMockitoTest.java
+++ src/test/java/kea/sem3/jwtdemo/service/CarServiceMockitoTest.java
@@ -19,7 +19,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 
 @ExtendWith(MockitoExtension.class)
-class CarServiceTestMockito {
+class CarServiceMockitoTest {
 
     @Mock
     CarRepository carRepository;
