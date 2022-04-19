a1e61b0dadfb2f1ebe7affe989c258ec88016ecf
plaul <lmor@kea.dk>
2022-04-19 18:11:30 +0200

    Ready to test Query Exercises

---
+++ src/test/java/kea/sem3/jwtdemo/repositories/ReservationRepositoryTest.java
@@ -0,0 +1,56 @@
+package kea.sem3.jwtdemo.repositories;
+
+import kea.sem3.jwtdemo.entity.Car;
+import kea.sem3.jwtdemo.entity.Member;
+import kea.sem3.jwtdemo.entity.Reservation;
+import kea.sem3.jwtdemo.entity.Role;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+class ReservationRepositoryTest {
+
+  @Autowired
+  CarRepository carRepository;
+
+  @Autowired
+  MemberRepository memberRepository;
+
+  @Autowired
+  ReservationRepository reservationRepository;
+
+  //Store som id's for the test methods
+  static Car carRav4,carV40;
+  static LocalDate reservationDate = LocalDate.of(2022, 3, 1);
+
+  @BeforeAll
+  static void setUp(@Autowired CarRepository carRepository,
+                    @Autowired MemberRepository memberRepository,
+                    @Autowired ReservationRepository reservationRepository) {
+    reservationRepository.deleteAll();
+    carRepository.deleteAll();
+    memberRepository.deleteAll();
+    carRav4 = carRepository.save(new Car("Toyota","RAV4",900,10));
+    carV40 = carRepository.save(new Car("Volvo","V40",700,10));
+    //Two Cars with no reservations
+    carRepository.save(new Car("Suzuki", "Vitara", 500, 14));
+    carRepository.save(new Car("Suzuki", "Vitara", 500, 14));
+
+    Member m1 = memberRepository.save(new Member("kw", "kw@a.dk", "test12", "Kurt", "Wonnegut", "Lyngbyvej 34", "Lyngby", "2800"));
+    reservationRepository.save(new Reservation(reservationDate, carRav4, m1));
+    reservationRepository.save(new Reservation(reservationDate, carV40, m1));
+    reservationRepository.save(new Reservation(LocalDate.of(2022,3,2), carRav4, m1));
+  }
+
+  @Test
+  public void aTest(){
+    assertTrue(true);
+  }
+}
